d48a10c51fb35634252812622b2f2ee65263ea47
clemente <clementeahuatl@gmail.com>
2025-07-03 14:30:26 -0400

    Added a warning text when inputs are missing.

--- index.js
+++ index.js
@@ -3,18 +3,21 @@ let passwordInput = document.getElementById("password");
 let emailTag = document.getElementById("emailTag");
 let passwordTag = document.getElementById("passwordTag");
 
+let warningShow = document.getElementById("warningEmail");
+let warningPwd = document.getElementById("warningPassword");
+
 function isInputsEmpty(){
     let emailValue = emailInput.value;
     let passwordValue = passwordInput.value;
-    
-    let emailTrim = emailValue.trim();
-    let passwordTrim = passwordValue.trim();
     
     if(emailInput.value == '' && passwordInput.value == ''){
 	    console.log("something");
 	    emailInput.style.outline = "solid 3px var(--danger)";
 	    emailInput.style.borderRadius = "20px"
 	    emailInput.style.borderBottom = "none";
+	    
+	    warningShow.style.display = "block";
+	    warningPwd.style.display = "block";
 	    
 	    passwordInput.style.outline = "solid 3px var(--danger)";
 	    passwordInput.style.borderRadius = "20px"
@@ -29,13 +32,16 @@ function isInputsEmpty(){
 	    emailInput.style.borderRadius = "20px"
 	    emailInput.style.borderBottom = "none";
 	    
-	     emailTag.style.color = "var(--danger)";
+	    emailTag.style.color = "var(--danger)";
+	    warningShow.style.display = "block";
+	    warningPassword.style.display = "block";
 	    	
 	}else if(passwordInput.value == ''){
         passwordInput.style.outline = "solid 3px var(--danger)";
 	    passwordInput.style.borderRadius = "20px"
 	    passwordInput.style.borderBottom = "none";
 	    
-	    passwordTag.style.color = "var(--danger)";	
+	    passwordTag.style.color = "var(--danger)";
+	    warningPwd.style.display = "block";	
 	}
 }
